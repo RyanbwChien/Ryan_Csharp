11b062d2e7cb0e7233065d60231d1feac80c3309
DESKTOP-OQJ6D6B\Ryan <saikidd2002@gmail.com>
2025-08-17 21:43:10 +0800

    no message

--- C_Sharp/MyConsoleApp/test.cs
+++ C_Sharp/MyConsoleApp/test.cs
@@ -1,36 +1,45 @@
 using System;
+using System.ComponentModel;
 
 public class Mytest
 {
     public int x = 1;
     public int y = 2;
+    public bool b = false;
     public static int func(int a, int b)
     {
         return a + b;
+    }
+    public int func2(int a, int b)
+    {
+        return b*a + b+a;
     }
 }
 
 
 
-class Myprogram
+public class Myprogram
 {
-    static void Main()
+    static void Main(String[] args)
     {
         // Mytest RR;
         int[] number = { 5, 9, 6 };
+        Mytest Mytest_Instance = new Mytest();
         for (int i = 0; i <= 5; i++)
         {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine("Hello World");
-                }
-                
-                else
-                {
-                    Console.WriteLine("Hello World");
-                    Console.WriteLine(Mytest.func(5, 2));
-                    
-                }
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("Hello World");
+            }
+
+            else
+            {
+                Console.WriteLine("Hello World");
+                Console.WriteLine(Mytest.func(5, 2));
+                Console.WriteLine(Mytest_Instance.func2(5, 2));
+                string input = Console.ReadLine();
+                Console.WriteLine("PRINT RESULT" + input);
+            }
 
         }
 
